2c5000a818df20e0821c58ea6a0b3aefff77c153
Liantsoa Rasata <rmliantsoa@gmail.com>
2025-09-30 20:25:22 +0200

    chore: bump image-uploader to v1.0.21

--- image_uploader.tf
+++ image_uploader.tf
@@ -1,5 +1,5 @@
 module "image_uploader" {
-  source = "git::https://github.com/lrasata/infra-image-uploader//modules/image_uploader?ref=v1.0.20"
+  source = "git::https://github.com/lrasata/infra-image-uploader//modules/image_uploader?ref=v1.0.21"
 
   lambda_process_uploaded_file_dir = "./lambda_process_uploaded_file_build"
 
