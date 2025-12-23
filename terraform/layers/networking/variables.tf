53174426e420c15e539706d1a7677da8eb9d78f6
Liantsoa Rasata <rmliantsoa@gmail.com>
2025-12-23 16:05:08 +0100

    feat: deployment of networking layer on ephemeral env

--- terraform/layers/networking/variables.tf
+++ terraform/layers/networking/variables.tf
@@ -1,13 +1,11 @@
 variable "environment" {
   description = "The environment for the deployment (e.g., dev, staging, prod)"
   type        = string
-  default     = "prod"
 }
 
 variable "region" {
   description = "The AWS region to deploy resources"
   type        = string
-  default     = "eu-central-1"
 }
 
 variable "app_id" {
@@ -18,23 +16,19 @@ variable "app_id" {
 variable "azs" {
   description = "List of availability zones in the region"
   type        = list(string)
-  default     = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
 }
 
 variable "vpc_cidr" {
   description = "The CIDR block for the VPC"
   type        = string
-  default     = "10.0.0.0/16"
 }
 
 variable "public_subnets_ips" {
   description = "List of public subnet CIDR blocks"
   type        = list(string)
-  default     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 }
 
 variable "private_subnets_ips" {
   description = "List of public subnet CIDR blocks"
   type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
