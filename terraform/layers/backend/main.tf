f03a8f45396c547939d0518ef42f2942e9c771c6
Liantsoa Rasata <rmliantsoa@gmail.com>
2026-01-06 16:05:41 +0100

    fix: encryption on s3_image_moderatora quarantine bucket

--- terraform/layers/backend/main.tf
+++ terraform/layers/backend/main.tf
@@ -83,10 +83,11 @@ module "file_uploader" {
 }
 
 module "image_moderator" {
-  source = "git::https://github.com/lrasata/infra-s3-image-moderator//modules/s3_image_moderator?ref=v1.1.0"
+  source = "git::https://github.com/lrasata/infra-s3-image-moderator//modules/s3_image_moderator?ref=main"
 
   region                    = var.region
   environment               = var.environment
+  app_id                    = var.app_id
   s3_src_bucket_name        = module.file_uploader.uploads_bucket_id
   s3_src_bucket_arn         = module.file_uploader.uploads_bucket_arn
   s3_quarantine_bucket_name = "${var.quarantine_bucket_name}-${data.aws_caller_identity.current.account_id}"
