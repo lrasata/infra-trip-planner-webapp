903bad4c3e1ae0d2393726a9dd3b0e58efeddc61
Liantsoa Rasata <rmliantsoa@gmail.com>
2026-01-07 16:03:21 +0100

    fix: provide cidr block in security group not subnet ids

--- terraform/layers/networking/outputs.tf
+++ terraform/layers/networking/outputs.tf
@@ -9,3 +9,12 @@ output "private_subnets" {
 output "public_subnets" {
   value = module.vpc.public_subnets
 }
+
+
+output "private_subnet_cidrs" {
+  value = module.vpc.private_subnets_cidr_blocks
+}
+
+output "public_subnet_cidrs" {
+  value = module.vpc.public_subnets_cidr_blocks
+}
